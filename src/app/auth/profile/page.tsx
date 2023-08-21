aa7eb78c59cb27232c32570759a62a15a5a0f5f1
Zidane <muhzidanealsaadawi@gmail.com>
2023-08-21 14:37:17 +0700

    Feature: Init husky

--- src/app/auth/profile/page.tsx
+++ src/app/auth/profile/page.tsx
@@ -1,5 +1,7 @@
-export default function AuthProfike() {
-  return (
-    <h1>Auth Profile !</h1>
-  )
-}
+import React from "react";
+
+const AuthProfile = (): React.ReactElement => {
+  return <h1>Auth Profile !</h1>;
+};
+
+export default AuthProfile;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
-export default function Home() {
+const Home = () => {
   return <div>Aku Maung</div>;
-}
+};
+
+export default Home;
